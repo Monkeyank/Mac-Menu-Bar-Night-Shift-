10d143da8e212a90d877ff9cbf40b00d1e48e795
Ankith Madadi <ankithm28@gmail.com>
2020-02-17 00:18:33 -0800

    Init

---
+++ Night-Shift/Night-Shift/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Night-Shift
+//
+//  Created by Ankith on 2/17/20.
+//  Copyright © 2020 Ankith. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
